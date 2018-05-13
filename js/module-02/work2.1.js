38f95b9c27c9cb80f1810e03832cb20740c84c92
Ivka88 <35501933+Ivka88@users.noreply.github.com>
2018-05-13 20:12:39 +0300

    1

--- js/module-02/work2.1.js
+++ js/module-02/work2.1.js
@@ -1,23 +1,21 @@
-
-"use strict"
-
+"use strict";
 
 const numbers = [];
 let userInput;
 while (true) {
-let userInput = prompt("Введите числo");
-console.log(userInput);
-if (userInput !== null && !isNaN(userInput)) numbers.push(+userInput);
-console.log (numbers);
-if (userInput === "" || userInput === null ) break;
-if (!isNaN(userInput)) {
-} else {
-alert ("Было введено не число, попробуйте еще раз");
-}
+  let userInput = prompt("Введите числo");
+  if (!isNaN(userInput)) {
+    numbers.push(+userInput);
+    console.log(numbers);
+  } else {
+    alert("Было введено не число, попробуйте еще раз");
+  }
+
+  if (userInput === "" || userInput === null) break;
 }
 let total = 0;
 for (let i = 0; i < numbers.length; i += 1) {
-total += numbers[i];
-} 
+  total += numbers[i];
+}
 alert(`Общая сумма чисел равна ${total}`);
-console.log (total);
+console.log(total);
